a0f533e556536bd0ebcb5268ded143e1d9ae8e86
SheethalShobi <sheethalshobi@gmail.com>
2023-10-01 23:02:44 +0530

    Initial commit

---
+++ src/homeproduct.js
@@ -0,0 +1,125 @@
+import tab from "./img/tp1.jpg";
+import smartwatch from "./img/tp2.jpg";
+import headphone from "./img/tp3.jpg";
+import camera from "./img/tp4.jpg";
+import powerbank from "./img/tp5.jpg";
+import cpu from "./img/tp6.png";
+import tp7 from "./img/tp7.jpg";
+import tp8 from "./img/tp8.jpg";
+import p9 from "./img/p9.jpg";
+import p10 from "./img/p10.jpg";
+import p11 from "./img/p11.jpg";
+import p12 from "./img/p12.jpg";
+import lap from "./img/lap.png";
+import smartband from "./img/smartband.jpg";
+import earpods from "./img/earpods.jpg";
+
+const Homeproduct = 
+[
+    {
+        id:1,
+        Title:"Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.",
+        Cat: 'Tablet',
+        Price: '723',
+        Img: tab
+    },
+    {
+        id:2,
+        Title:"Tracker with IP67 Waterproof Pedometer Smart watch.",
+        Cat: 'Smart Watch',
+        Price: '168',
+        Img: smartwatch
+    },
+    {
+        id:3,
+        Title:"Cancelling Headphones Wireless.",
+        Cat: 'Headphone',
+        Price: '49',
+        Img: headphone
+    },
+    {
+        id:4,
+        Title:"Professional Camera 4K Digital Video Camera.",
+        Cat: 'Camera',
+        Price: '1049',
+        Img: camera
+    },
+    {
+        id:5,
+        Title:"Mini Portable PD 22.5W Fast Charging Powerbank.",
+        Cat: 'Powerbank',
+        Price: '49',
+        Img:powerbank
+    },
+    {
+        id:6,
+        Title:"CPU Cooler 2 Heat Pipes 12mm 4 Pin PWM RGB for Intel.",
+        Cat: 'Heat pipe',
+        Price: '156',
+        Img:cpu
+    },
+    {
+        id:7,
+        Title:"Playstation 4 2TB Slim Gaming Console.",
+        Cat: 'Gaming',
+        Price: '2098',
+        Img:tp7
+    },
+    {
+        id:8,
+        Title:"Mini Portable Mobile Phone Powerbank for iphone.",
+        Cat: 'Powerbank',
+        Price: '386',
+        Img: tp8
+    },
+    {
+        id:9,
+        Title:"Microsoft Surface Pro 8-13' Touchscreen.",
+        Cat: 'Tablet',
+        Price: '693',
+        Img: p9
+    },
+    {
+        id:10,
+        Title:"Playstation 4 2TB Slim Gaming Console.",
+        Cat: 'Gaming',
+        Price: '5036',
+        Img: p10
+    },
+    {
+        id:11,
+        Title:"Echo Show 5 (2nd Gen) Adjustable Stand | Charcoal",
+        Cat: 'Electronics',
+        Price: '198',
+        Img: p11
+    },
+    {
+        id:12,
+        Title:"Echo Dot smart speaker",
+        Cat: 'Electronics',
+        Price: '793',
+        Img: p12
+    },
+    {
+        id:13,
+        Title:"Xiaomi Mi Band 2 Smartwatch",
+        Cat: 'Electronics',
+        Price: '5036',
+        Img: smartband
+    },
+    {
+        id:14,
+        Title:"Apple Wireless earpods",
+        Cat: 'Electronics',
+        Price: '8198',
+        Img: earpods
+    },
+    {
+        id:15,
+        Title:"Asus Vivobook Laptops",
+        Cat: 'Electronics',
+        Price: '45593',
+        Img: lap
+    },
+]
+export default Homeproduct
